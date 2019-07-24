b51c9aed1542c922c70ad20022de0959e212a6de
Deepak Vangala <44211489+dvangala21@users.noreply.github.com>
2019-07-23 21:13:16 -0500

    retry

---
+++ static/js/data.js
@@ -0,0 +1,800 @@
+var data = [
+    {
+        "Song": "i cant help myself sugar pie honey bunch",
+        "Artist": "four tops",
+        "Year": 1965,
+        "Schmood": "love"
+    },
+    {
+        "Song": "mr tambourine man",
+        "Artist": "the byrds",
+        "Year": 1965,
+        "Schmood": "sad"
+    },
+    {
+        "Song": "hang on sloopy",
+        "Artist": "the mccoys",
+        "Year": 1965,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "ticket to ride",
+        "Artist": "the beatles",
+        "Year": 1965,
+        "Schmood": "sad"
+    },
+    {
+        "Song": "tired of waiting for you",
+        "Artist": "the kinks",
+        "Year": 1965,
+        "Schmood": "angry"
+    },
+    {
+        "Song": "california dreamin",
+        "Artist": "the mamas the papas",
+        "Year": 1966,
+        "Schmood": "saucy"
+    },
+    {
+        "Song": "strangers in the night",
+        "Artist": "frank sinatra",
+        "Year": 1966,
+        "Schmood": "love"
+    },
+    {
+        "Song": "hanky panky",
+        "Artist": "tommy james and the shondells",
+        "Year": 1966,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "paint it black",
+        "Artist": "the rolling stones",
+        "Year": 1966,
+        "Schmood": "saucy"
+    },
+    {
+        "Song": "daydream",
+        "Artist": "the lovin spoonful",
+        "Year": 1966,
+        "Schmood": "saucy"
+    },
+    {
+        "Song": "crying time",
+        "Artist": "ray charles",
+        "Year": 1966,
+        "Schmood": "sad"
+    },
+    {
+        "Song": "the sound of silence",
+        "Artist": "simon garfunkel",
+        "Year": 1966,
+        "Schmood": "sad"
+    },
+    {
+        "Song": "barbara ann",
+        "Artist": "the beach boys",
+        "Year": 1966,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "nowhere man",
+        "Artist": "the beatles",
+        "Year": 1966,
+        "Schmood": "saucy"
+    },
+    {
+        "Song": "happy together",
+        "Artist": "the turtles",
+        "Year": 1967,
+        "Schmood": "love"
+    },
+    {
+        "Song": "respect",
+        "Artist": "aretha franklin",
+        "Year": 1967,
+        "Schmood": "angry"
+    },
+    {
+        "Song": "for what its worth",
+        "Artist": "buffalo springfield",
+        "Year": 1967,
+        "Schmood": "angry"
+    },
+    {
+        "Song": "somebody to love",
+        "Artist": "jefferson airplane",
+        "Year": 1967,
+        "Schmood": "saucy"
+    },
+    {
+        "Song": "friday on my mind",
+        "Artist": "the easybeats",
+        "Year": 1967,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "i never loved a man the way i love you",
+        "Artist": "aretha franklin",
+        "Year": 1967,
+        "Schmood": "angry"
+    },
+    {
+        "Song": "white rabbit",
+        "Artist": "jefferson airplane",
+        "Year": 1967,
+        "Schmood": "saucy"
+    },
+    {
+        "Song": "the beat goes on",
+        "Artist": "sonny cher",
+        "Year": 1967,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "sunshine of your love",
+        "Artist": "cream",
+        "Year": 1968,
+        "Schmood": "love"
+    },
+    {
+        "Song": "lady willpower",
+        "Artist": "gary puckett the union gap",
+        "Year": 1968,
+        "Schmood": "love"
+    },
+    {
+        "Song": "jumpin jack flash",
+        "Artist": "the rolling stones",
+        "Year": 1968,
+        "Schmood": "angry"
+    },
+    {
+        "Song": "light my fire",
+        "Artist": "jose feliciano",
+        "Year": 1968,
+        "Schmood": "love"
+    },
+    {
+        "Song": "sky pilot",
+        "Artist": "the animals",
+        "Year": 1968,
+        "Schmood": "sad"
+    },
+    {
+        "Song": "revolution",
+        "Artist": "the beatles",
+        "Year": 1968,
+        "Schmood": "angry"
+    },
+    {
+        "Song": "white room",
+        "Artist": "cream",
+        "Year": 1968,
+        "Schmood": "saucy"
+    },
+    {
+        "Song": "say it loud im black and im proud",
+        "Artist": "james brown",
+        "Year": 1968,
+        "Schmood": "angry"
+    },
+    {
+        "Song": "sugar sugar",
+        "Artist": "the archies",
+        "Year": 1969,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "one",
+        "Artist": "three dog night",
+        "Year": 1969,
+        "Schmood": "sad"
+    },
+    {
+        "Song": "suspicious minds",
+        "Artist": "elvis presley",
+        "Year": 1969,
+        "Schmood": "love"
+    },
+    {
+        "Song": "proud mary",
+        "Artist": "creedence clearwater revival",
+        "Year": 1969,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "sweet caroline",
+        "Artist": "neil diamond",
+        "Year": 1969,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "love can make you happy",
+        "Artist": "mercy",
+        "Year": 1969,
+        "Schmood": "love"
+    },
+    {
+        "Song": "soul deep",
+        "Artist": "the box tops",
+        "Year": 1969,
+        "Schmood": "love"
+    },
+    {
+        "Song": "hooked on a feeling",
+        "Artist": "bj thomas",
+        "Year": 1969,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "american woman",
+        "Artist": "the guess who",
+        "Year": 1970,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "let it be",
+        "Artist": "the beatles",
+        "Year": 1970,
+        "Schmood": "sad"
+    },
+    {
+        "Song": "ride captain ride",
+        "Artist": "blues image",
+        "Year": 1970,
+        "Schmood": "saucy"
+    },
+    {
+        "Song": "fire and rain",
+        "Artist": "james taylor",
+        "Year": 1970,
+        "Schmood": "sad"
+    },
+    {
+        "Song": "without love there is nothing",
+        "Artist": "tom jones",
+        "Year": 1970,
+        "Schmood": "sas"
+    },
+    {
+        "Song": "the thrill is gone",
+        "Artist": "bb king",
+        "Year": 1970,
+        "Schmood": "sad"
+    },
+    {
+        "Song": "tired of being alone",
+        "Artist": "al green",
+        "Year": 1971,
+        "Schmood": "sad"
+    },
+    {
+        "Song": "for all we know",
+        "Artist": "the carpenters",
+        "Year": 1971,
+        "Schmood": "love"
+    },
+    {
+        "Song": "mercy mercy me the ecology",
+        "Artist": "marvin gaye",
+        "Year": 1971,
+        "Schmood": "angry"
+    },
+    {
+        "Song": "i just want to celebrate",
+        "Artist": "rare earth",
+        "Year": 1971,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "wont get fooled again",
+        "Artist": "the who",
+        "Year": 1971,
+        "Schmood": "angry"
+    },
+    {
+        "Song": "lets stay together",
+        "Artist": "al green",
+        "Year": 1972,
+        "Schmood": "love"
+    },
+    {
+        "Song": "rocket man",
+        "Artist": "elton john",
+        "Year": 1972,
+        "Schmood": "sad"
+    },
+    {
+        "Song": "never been to spain",
+        "Artist": "three dog night",
+        "Year": 1972,
+        "Schmood": "saucy"
+    },
+    {
+        "Song": "get on the good foot",
+        "Artist": "james brown",
+        "Year": 1972,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "pop that thang",
+        "Artist": "the isley brothers",
+        "Year": 1972,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "my love",
+        "Artist": "paul mccartney wings",
+        "Year": 1973,
+        "Schmood": "love"
+    },
+    {
+        "Song": "right place wrong time",
+        "Artist": "dr john",
+        "Year": 1973,
+        "Schmood": "saucy"
+    },
+    {
+        "Song": "love train",
+        "Artist": "the ojays",
+        "Year": 1973,
+        "Schmood": "love"
+    },
+    {
+        "Song": "live and let die",
+        "Artist": "paul mccartney wings",
+        "Year": 1973,
+        "Schmood": "sad"
+    },
+    {
+        "Song": "gypsy man",
+        "Artist": "war",
+        "Year": 1973,
+        "Schmood": "saucy"
+    },
+    {
+        "Song": "the way we were",
+        "Artist": "barbra streisand",
+        "Year": 1974,
+        "Schmood": "sad"
+    },
+    {
+        "Song": "jungle boogie",
+        "Artist": "kool the gang",
+        "Year": 1974,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "the joker",
+        "Artist": "steve miller band",
+        "Year": 1974,
+        "Schmood": "saucy"
+    },
+    {
+        "Song": "rockin roll baby",
+        "Artist": "the stylistics",
+        "Year": 1974,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "wild thing",
+        "Artist": "fancy",
+        "Year": 1974,
+        "Schmood": "love"
+    },
+    {
+        "Song": "laughter in the rain",
+        "Artist": "neil sedaka",
+        "Year": 1975,
+        "Schmood": "love"
+    },
+    {
+        "Song": "cats in the cradle",
+        "Artist": "harry chapin",
+        "Year": 1975,
+        "Schmood": "sad"
+    },
+    {
+        "Song": "you are so beautiful",
+        "Artist": "joe cocker",
+        "Year": 1975,
+        "Schmood": "love"
+    },
+    {
+        "Song": "killer queen",
+        "Artist": "queen",
+        "Year": 1975,
+        "Schmood": "saucy"
+    },
+    {
+        "Song": "bungle in the jungle",
+        "Artist": "jethro tull",
+        "Year": 1975,
+        "Schmood": "saucy"
+    },
+    {
+        "Song": "swearin to god",
+        "Artist": "frankie valli",
+        "Year": 1975,
+        "Schmood": "love"
+    },
+    {
+        "Song": "shake it up",
+        "Artist": "the cars",
+        "Year": 1982,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "let it whip",
+        "Artist": "dazz band",
+        "Year": 1982,
+        "Schmood": "love"
+    },
+    {
+        "Song": "we got the beat",
+        "Artist": "the gogos",
+        "Year": 1982,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "the other woman",
+        "Artist": "ray parker jr",
+        "Year": 1982,
+        "Schmood": "lonely"
+    },
+    {
+        "Song": "only the lonely",
+        "Artist": "the motels",
+        "Year": 1982,
+        "Schmood": "lonely"
+    },
+    {
+        "Song": "who can it be now",
+        "Artist": "men at work",
+        "Year": 1982,
+        "Schmood": "angry"
+    },
+    {
+        "Song": "hold me",
+        "Artist": "fleetwood mac",
+        "Year": 1982,
+        "Schmood": "lonely"
+    },
+    {
+        "Song": "eye in the sky",
+        "Artist": "the alan parsons project",
+        "Year": 1982,
+        "Schmood": "angry"
+    },
+    {
+        "Song": "lets groove",
+        "Artist": "earth wind fire",
+        "Year": 1982,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "open arms",
+        "Artist": "journey",
+        "Year": 1982,
+        "Schmood": "love"
+    },
+    {
+        "Song": "leather and lace",
+        "Artist": "stevie nicks and don henley",
+        "Year": 1982,
+        "Schmood": "love"
+    },
+    {
+        "Song": "loves been a little bit hard on me",
+        "Artist": "juice newton",
+        "Year": 1982,
+        "Schmood": "love"
+    },
+    {
+        "Song": "i feel for you",
+        "Artist": "chaka khan",
+        "Year": 1985,
+        "Schmood": "happy"
+    },
+    {
+        "Song": "out of touch",
+        "Artist": "daryl hall john oates",
+        "Year": 1985,
+        "Schmood": "lonely"
+    },
+    {
+        "Song": "everybody wants to rule the world",
+        "Artist": "tears for fears",
+        "Year": 1985,
+        "Schmood": "spiritual"
+    },
+    {
+        "Song": "crazy for you",
+        "Artist": "madonna",
+        "Year": 1985,
+        "Schmood": "love"
+    },
+    {
+        "Song": "everytime you go away",
+        "Artist": "paul young",
+        "Year": 1985,
+        "Schmood": "lonely"
+    },
+    {
+        "Song": "the wild boys",
+        "Artist": "duran duran",
+        "Year": 1985,
+        "Schmood": "saucy"
+    },
+    {
+        "Song": "youre the inspiration",
+        "Artist": "chicago",
+        "Year": 1985,
+        "Schmood": "love"
+    },
+    {
+        "Song": "neutron dance",
+        "Artist": "the pointer sisters",
+        "Year": 1985,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "all i need",
+        "Artist": "jack wagner",
+        "Year": 1985,
+        "Schmood": "saucy"
+    },
+    {
+        "Song": "freeway of love",
+        "Artist": "aretha franklin",
+        "Year": 1985,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "never surrender",
+        "Artist": "corey hart",
+        "Year": 1985,
+        "Schmood": "sad"
+    },
+    {
+        "Song": "separate lives",
+        "Artist": "phil collins and marilyn martin",
+        "Year": 1985,
+        "Schmood": "sad"
+    },
+    {
+        "Song": "one night in bangkok",
+        "Artist": "murray head",
+        "Year": 1985,
+        "Schmood": "saucy"
+    },
+    {
+        "Song": "smooth operator",
+        "Artist": "sade",
+        "Year": 1985,
+        "Schmood": "saucy"
+    },
+    {
+        "Song": "in my house",
+        "Artist": "mary jane girls",
+        "Year": 1985,
+        "Schmood": "saucy"
+    },
+    {
+        "Song": "dont lose my number",
+        "Artist": "phil collins",
+        "Year": 1985,
+        "Schmood": "love"
+    },
+    {
+        "Song": "all through the night",
+        "Artist": "cyndi lauper",
+        "Year": 1985,
+        "Schmood": "lonely"
+    },
+    {
+        "Song": "valotte",
+        "Artist": "julian lennon",
+        "Year": 1985,
+        "Schmood": "sad"
+    },
+    {
+        "Song": "some like it hot",
+        "Artist": "power station",
+        "Year": 1985,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "solid",
+        "Artist": "ashford simpson",
+        "Year": 1985,
+        "Schmood": "love"
+    },
+    {
+        "Song": "lonely ol night",
+        "Artist": "john cougar mellencamp",
+        "Year": 1985,
+        "Schmood": "lonely"
+    },
+    {
+        "Song": "what about love",
+        "Artist": "heart",
+        "Year": 1985,
+        "Schmood": "lonely"
+    },
+    {
+        "Song": "california girls",
+        "Artist": "david lee roth",
+        "Year": 1985,
+        "Schmood": "saucy"
+    },
+    {
+        "Song": "fresh",
+        "Artist": "kool the gang",
+        "Year": 1985,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "do what you do",
+        "Artist": "jermaine jackson",
+        "Year": 1985,
+        "Schmood": "lonely"
+    },
+    {
+        "Song": "release me",
+        "Artist": "wilson phillips",
+        "Year": 1990,
+        "Schmood": "sad"
+    },
+    {
+        "Song": "rub you the right way",
+        "Artist": "johnny gill",
+        "Year": 1990,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "ready or not",
+        "Artist": "after 7",
+        "Year": 1990,
+        "Schmood": "love"
+    },
+    {
+        "Song": "something happened on the way to heaven",
+        "Artist": "phil collins",
+        "Year": 1990,
+        "Schmood": "sad"
+    },
+    {
+        "Song": "king of wishful thinking",
+        "Artist": "go west",
+        "Year": 1990,
+        "Schmood": "sad"
+    },
+    {
+        "Song": "its just the way that you love me",
+        "Artist": "paula abdul",
+        "Year": 1990,
+        "Schmood": "love"
+    },
+    {
+        "Song": "make you sweat",
+        "Artist": "keith sweat",
+        "Year": 1990,
+        "Schmood": "saucy"
+    },
+    {
+        "Song": "swing the mood",
+        "Artist": "jive bunny and the mastermixers",
+        "Year": 1990,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "more than words",
+        "Artist": "extreme",
+        "Year": 1991,
+        "Schmood": "love"
+    },
+    {
+        "Song": "all the man that i need",
+        "Artist": "whitney houston",
+        "Year": 1991,
+        "Schmood": "sad"
+    },
+    {
+        "Song": "good vibrations",
+        "Artist": "marky mark and the funky bunch featuring loleatta holloway",
+        "Year": 1991,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "sensitivity",
+        "Artist": "ralph tresvant",
+        "Year": 1991,
+        "Schmood": "love"
+    },
+    {
+        "Song": "here we go",
+        "Artist": "cc music factory",
+        "Year": 1991,
+        "Schmood": "saucy"
+    },
+    {
+        "Song": "fading like a flower every time you leave",
+        "Artist": "roxette",
+        "Year": 1991,
+        "Schmood": "sad"
+    },
+    {
+        "Song": "love is a wonderful thing",
+        "Artist": "michael bolton",
+        "Year": 1991,
+        "Schmood": "love"
+    },
+    {
+        "Song": "i touch myself",
+        "Artist": "divinyls",
+        "Year": 1991,
+        "Schmood": "love"
+    },
+    {
+        "Song": "after the rain",
+        "Artist": "nelson",
+        "Year": 1991,
+        "Schmood": "love"
+    },
+    {
+        "Song": "3 am eternal",
+        "Artist": "the klf",
+        "Year": 1991,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "cream",
+        "Artist": "prince and the new power generation",
+        "Year": 1991,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "here i am come and take me",
+        "Artist": "ub40",
+        "Year": 1991,
+        "Schmood": "love"
+    },
+    {
+        "Song": "silent lucidity",
+        "Artist": "queensr\"yche",
+        "Year": 1991,
+        "Schmood": "sad"
+    },
+    {
+        "Song": "ill make love to you",
+        "Artist": "boyz ii men",
+        "Year": 1994,
+        "Schmood": "love"
+    },
+    {
+        "Song": "any time any place",
+        "Artist": "janet jackson",
+        "Year": 1994,
+        "Schmood": "love"
+    },
+    {
+        "Song": "linger",
+        "Artist": "the cranberries",
+        "Year": 1994,
+        "Schmood": "angry"
+    },
+    {
+        "Song": "groove thang",
+        "Artist": "zhane",
+        "Year": 1994,
+        "Schmood": "dance"
+    },
+    {
+        "Song": "gangstas paradise",
+        "Artist": "coolio featuring lv",
+        "Year": 1995,
+        "Schmood": "sad"
+    }
+]
