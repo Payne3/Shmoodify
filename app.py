78ddda54c4e29f8aa4eae0c8ab6e103288d3c73c
William Payne <williampayne831@gmail.com>
2019-07-27 12:15:54 -0500

    adding about page link to heroku

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
