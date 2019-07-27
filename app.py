7496b4ec306586f97be0b1bceaf7e26e757d7354
William Payne <williampayne831@gmail.com>
2019-07-27 15:14:00 -0500

    one more time

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, jsonify, request, redirect, Response
 app = Flask(__name__, template_folder='templates')
 
 
-@app.route('/')
+@app.route('/index.html')
 def home():
     return render_template('index.html')
 
